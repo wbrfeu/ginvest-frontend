cc69065f37d73af777b7342c354c85f9dc351b5d
walter <wbrfeu@gmail.com>
2021-06-25 16:51:11 -0300

    captura url

--- src/constantes/urls.js
+++ src/constantes/urls.js
@@ -1,21 +1,16 @@
 // Caminhos do Ambiente de DESENVOLVIMENTO
 const urlBackendDesenv = "http://localhost:2000"
-const urlFrontendDesenv = "http://localhost:3000"
 
 // Caminhos do Ambiente de PRODUÇÃO
 const urlBackendProd = "http://34.68.109.223:2000"
-const urlFrontendProd = "http://localhost:9050"
 
 // Verifica em qual ambiente o frontend está rodando e aponta para o backend correto
 let urlBackend = null
-let urlFrontend = null
 if (process.env.NODE_ENV === "development") {
     urlBackend = urlBackendDesenv
-    urlFrontend = urlFrontendDesenv
 }
 else if (process.env.NODE_ENV === "production") {
     urlBackend = urlBackendProd
-    urlFrontend = urlFrontendProd
 }
 
 const urlBackendLogin = urlBackend + "/login/google"
@@ -25,6 +20,9 @@ const urlBackendInvestimentos = urlBackend + "/investimentos"
 const urlBackendSalvaNovaNotaNegoc = urlBackend + "/td/novann"
 
 const urlBackendTabUteis = urlBackend + "/tabelasuteis"
+
+// Captura o Endereço onde o Frontend Está Rodando
+const urlFrontend = window.location.origin
 
 export {
     urlBackendLogin,
